38e652711589964553e72202de57e851d0aa03f3
Camilo Aguilar <camilo.aguilar@gmail.com>
2012-03-29 14:00:17 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	npm install
+
+.PHONY: install
---
+++ server.js
@@ -0,0 +1,32 @@
+
+var WebSocketServer = require('ws').Server;
+
+var agents = require('./lib');
+
+var wss = new WebSocketServer({port: 8080});
+
+wss.on('connection', function(socket) {
+    socket.on('message', function(message) {
+    	//console.log(message);
+    	try {
+    		message = JSON.parse(message);
+    	} catch(e) {
+    		console.log(e);
+    		return;
+    	}
+    	var id = message.id;
+    	var command = message.method.split('.');
+    	var domain = agents[command[0]];
+    	var method = command[1];
+    	var params = message.params;
+    	
+    	if (!domain || !domain[method]) {
+    		console.error('%s is not implemented', message.method);
+    		return;
+    	}
+
+    	domain[method](params, function(response) {
+    		socket.send(JSON.stringify({ id: id, result: response }));
+    	});
+    });
+});
