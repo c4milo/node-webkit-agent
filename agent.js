932aed69d7e9efcd9539fe4406fcac18bdd1a7ec
Camilo Aguilar <camilo.aguilar@gmail.com>
2012-04-03 12:59:00 -0400

    Implements activation upon SIGUSR2 as well as standlone initiation.

--- agent.js
+++ agent.js
@@ -1,34 +1,61 @@
 
 var WebSocketServer = require('ws').Server;
-
 var agents = require('./lib');
+var wss; 
 
-var wss = new WebSocketServer({port: 8080});
+function start() {
+    wss = new WebSocketServer({port: 8080});
 
-wss.on('connection', function(socket) {
-    socket.on('message', function(message) {
-    	//console.log(message);
-    	try {
-    		message = JSON.parse(message);
-    	} catch(e) {
-    		console.log(e);
-    		return;
-    	}
-    	var id = message.id;
-    	var command = message.method.split('.');
-    	var domain = agents[command[0]];
-    	var method = command[1];
-    	var params = message.params;
-    	
-    	if (!domain || !domain[method]) {
-    		console.error('%s is not implemented', message.method);
-    		return;
-    	}
+    wss.on('connection', function(socket) {
+        socket.on('message', function(message) {
+           //console.log(message);
+           try {
+              message = JSON.parse(message);
+           } catch(e) {
+              console.log(e);
+              return;
+           }
+           
+           var id = message.id;
+           var command = message.method.split('.');
+           var domain = agents[command[0]];
+           var method = command[1];
+           var params = message.params;
+        
+            if (!domain || !domain[method]) {
+                console.error('%s is not implemented', message.method);
+                return;
+            }
 
-    	domain[method](params, function(result) {
-    		socket.send(JSON.stringify({ id: id, result: result }));
-    	}, function(event) {
-            socket.send(JSON.stringify(event));
+            domain[method](params, function(result) {
+                socket.send(JSON.stringify({ id: id, result: result }));
+            }, function(event) {
+                socket.send(JSON.stringify(event));
+            });
         });
     });
+}
+
+function stop() {
+    if (wss) {
+        wss.close();
+        wss = null;
+    } 
+}
+
+if (!module.parent) {
+    start();
+} else {
+    process.on('SIGUSR2', function() {
+        if (ws) {
+            stop();
+        } else {
+            start();
+        }
+    });
+}
+
+process.on('uncaughtException', function (err) {
+  console.error('Node inspector agent uncaughtException: ');
+  console.error(err.stack);
 });
