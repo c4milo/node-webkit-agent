b01ee132dbff4ceaeb7a8eaa3b45847ce55f52fc
Camilo Aguilar <camilo.aguilar@gmail.com>
2012-05-13 22:25:54 -0400

    wip

---
+++ index.js
@@ -0,0 +1,70 @@
+var agents = require('./lib');
+var fork = require('child_process').fork;
+
+var child = fork('./webkit-devtools-agent.js', process.argv);
+
+child.on('message', function(message) {
+    var data = message.data;
+    switch(message.event) {
+        case 'connection':
+            initializeAgents();
+            break;
+        case 'method':
+            var id = data.id;
+            var command = data.method.split('.');
+            var domain = agents[command[0]];
+            var method = command[1];
+            var params = data.params;
+
+            if (!domain || !domain[method]) {
+                console.warn('%s is not implemented', data.method);
+                return;
+            }
+
+            domain[method](params, function(result) {
+                child.send({
+                    event: 'result',
+                    data: {
+                        id: id,
+                        result: result
+                    }
+                });
+            });
+            break;
+    }
+});
+
+function initializeAgents() {
+    var sendEvent = function(data) {
+        child.send({
+            event: 'event',
+            data: data
+        });
+    };
+
+    var runtimeAgent = new agents.Runtime(sendEvent);
+
+    for (var agent in agents) {
+        if (typeof agents[agent] == 'function' &&
+            agent != 'Runtime') {
+                agents[agent] = new agents[agent](sendEvent, runtimeAgent);
+        }
+    }
+    agents['Runtime'] = runtimeAgent;
+}
+
+if (!module.parent) {
+    child.kill('SIGUSR2');
+} else {
+    process.on('SIGUSR2', function() {
+        child.kill('SIGUSR2');
+    });
+}
+
+child.on('exit', function(code, signal) {
+    console.log(code);
+    console.log(signal);
+    //TODO: re-spawn it
+});
+
+
--- webkit-devtools-agent.js
+++ webkit-devtools-agent.js
@@ -1,47 +1,44 @@
+var Debugger = require('./lib/debugger');
 var WebSocketServer = require('ws').Server;
-var agents = require('./lib');
 var wss;
 
-var port = process.env.DEBUG_PORT || 1337;
+var port = process.env.DEBUG_PORT || 9999;
 var host = process.env.DEBUG_HOST || '127.0.0.1';
 
 function start() {
-    wss = new WebSocketServer({port: port, host: host});
+    wss = new WebSocketServer({
+        port: port,
+        host: host
+    });
 
     console.log('webkit-devtools-agent started on %s:%s', host, port);
 
     wss.on('connection', function(socket) {
-        function sendEvent(event) {
-            socket.send(JSON.stringify(event));
-        }
+        process.send({
+            event: 'connection'
+        });
 
-        for (var agent in agents) {
-            if (typeof agents[agent] == 'function') {
-                agents[agent] = new agents[agent](sendEvent);
+        process.on('message', function(message) {
+            var data = message.data;
+
+            switch(message.event) {
+                case 'event':
+                case 'result':
+                    socket.send(JSON.stringify(data));
             }
-        }
+        });
 
         socket.on('message', function(message) {
             try {
                 message = JSON.parse(message);
             } catch(e) {
-                console.log(e);
+                console.error(e);
                 return;
             }
 
-            var id = message.id;
-            var command = message.method.split('.');
-            var domain = agents[command[0]];
-            var method = command[1];
-            var params = message.params;
-
-            if (!domain || !domain[method]) {
-                console.warn('%s is not implemented', message.method);
-                return;
-            }
-
-            domain[method](params, function(result) {
-                socket.send(JSON.stringify({ id: id, result: result }));
+            process.send({
+                event: 'method',
+                data: message
             });
         });
     });
@@ -50,24 +47,22 @@ function start() {
 function stop() {
     if (wss) {
         wss.close();
+        wss = null;
         console.log('webkit-devtools-agent stopped');
     }
 }
 
-if (!module.parent) {
-    start();
-} else {
-    process.on('SIGUSR2', function() {
-        if (wss) {
-            stop();
-        } else {
-            start();
-        }
-    });
-}
+process.on('SIGUSR2', function() {
+    if (wss) {
+        stop();
+    } else {
+        start();
+    }
+});
 
 process.on('uncaughtException', function (err) {
     console.error('webkit-devtools-agent: uncaughtException: ');
+    console.error(err);
     console.error(err.stack);
 });
 
